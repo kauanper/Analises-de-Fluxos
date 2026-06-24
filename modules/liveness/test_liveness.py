51bdde62ffc84520f07ca1391382381188af10ed
Savio Soares <saviocarvalho@alu.ufc.br>
2026-06-24 11:44:39 -0300

    liveness analysis

---
+++ modules/liveness/test_liveness.py
@@ -0,0 +1,36 @@
+from modules.liveness.liveness import run_liveness
+from modules.core.core import parse_input, print_result
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..')))
+
+
+def main():
+    example_input = [
+        "1 2",
+        "a = a+c",
+        "b = 4-a",
+        "2",
+        "2 1",
+        "b=20*c",
+        "3",
+        "3 2",
+        "d = a+b",
+        "b = 0",
+        "0"
+    ]
+
+    if len(sys.argv) > 1:
+        cfg = parse_input(sys.argv[1])
+    else:
+        cfg = parse_input(example_input)
+
+    result = run_liveness(cfg)
+
+    print_result("Liveness", result, cfg.block_order)
+
+
+if __name__ == "__main__":
+    main()
